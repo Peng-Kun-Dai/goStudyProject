304d7d45e9d5ee8a4cdd414224853affe66f7ba0
Peng-Kun-Dai <15202844993@163.com>
2019-05-06 16:58:25 +0800

    frist

--- src/hello/goStudy2019.5.6/xml.go
+++ src/hello/goStudy2019.5.6/xml.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+var t xml.Token
+
+func main() {
+	data, err := ioutil.ReadFile("./filetmp/people.xml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	reader := strings.NewReader((string(data)))
+	p := xml.NewDecoder(reader)
+	for t, err = p.Token(); err == nil; t, err = p.Token() {
+		switch token := t.(type) {
+		case xml.StartElement:
+			name := token.Name.Local
+			fmt.Printf("Token name: %s\n", name)
+			for _, attr := range token.Attr {
+				attrName := attr.Name.Local
+				attrValue := attr.Value
+				fmt.Printf("An attribute is: %s %s\n", attrName, attrValue)
+				// ...
+			}
+		case xml.EndElement:
+			fmt.Println("End of token")
+		case xml.CharData:
+			content := string([]byte(token))
+			fmt.Printf("This is the content: %v\n", content)
+			// ...
+		default:
+			// ...
+		}
+	}
+}
